0cae0901f7a65c44751321df67332133e27c7eb1
ysj_xdd <xdd@qq.com>
2017-08-15 14:20:40 +0800

    :wrench: 提交一些基础配置

---
+++ src/main/java/com/java/xdd/weixin/controller/WeixinController.java
@@ -0,0 +1,9 @@
+package com.java.xdd.weixin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping(value = "weixin")
+@Controller
+public class WeixinController {
+}
